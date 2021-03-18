bb743c21f19bb8c8057e094dc2bc41db0a38d762
brinehart <ben@brinehartdesign.com>
2021-03-18 00:51:51 -0400

    Add remote for VII to shell webpack config

--- angular/shell/webpack.config.js
+++ angular/shell/webpack.config.js
@@ -17,28 +17,18 @@ module.exports = {
   },
   plugins: [
     new ModuleFederationPlugin({
-      
-        // For remotes (please adjust)
-        // name: "shell",
-        // filename: "remoteEntry.js",
-        // exposes: {
-        //     './Component': './/src/app/app.component.ts',
-        // },        
-        
-        // For hosts (please adjust)
-        // remotes: {
-        //     "mfe1": "mfe1@http://localhost:3000/remoteEntry.js",
-
-        // },
+        remotes: {
+            "vii": "vii@http://localhost:7000/remoteEntry.js",
+        },
 
         shared: {
-          "@angular/core": { singleton: true, strictVersion: true }, 
-          "@angular/common": { singleton: true, strictVersion: true }, 
+          "@angular/core": { singleton: true, strictVersion: true },
+          "@angular/common": { singleton: true, strictVersion: true },
           "@angular/router": { singleton: true, strictVersion: true },
 
           ...sharedMappings.getDescriptors()
         }
-        
+
     }),
     sharedMappings.getPlugin(),
   ],
